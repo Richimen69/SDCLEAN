e8888577a1187d35cb1c5637f13edab649d55ded
Ricardo <xdricardo63@gmail.com>
2024-03-11 00:06:18 -0600

    Paginas Certificados y Servicios

--- src/views/certificados.jsx
+++ src/views/certificados.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Certificados = () => {
     return (
-        <div>Certificados</div>
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado1.jpg" className="img-fluid mb-3" />
+                    </div>
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado2.jpg" className="img-fluid mb-3" />
+                    </div>
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado3.jpg" className="img-fluid mb-3" />
+                    </div>
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado4.jpg" className="img-fluid mb-3" />
+                    </div>
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado5.jpg" className="img-fluid mb-3" />
+                    </div>
+                    <div className="col-md-6 mb-4">
+                        <img src="/src/assets/certificados/certificado6.jpg" className="img-fluid mb-3" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-export default Certificados
+
+export default Certificados;
